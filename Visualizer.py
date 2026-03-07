7e75d1f5ab4473b0ef30fab0445658119d45cc73
Potatovender <156006555+Potatovender@users.noreply.github.com>
2026-03-06 23:45:08 -0500

    Wrote starter notes

--- Visualizer.py
+++ Visualizer.py
@@ -1,1 +1,6 @@
-
+#Hi guys uh so idk exactly what can and should be here so uh
+#so we have everything pygame here
+# I suppose we also put like the list of equations and colors and all that jazz in here too?
+# And also like we look through and draw stuff in here too? 
+# So the Draw() function gets put here?
+#Yeah this file is gonna have a lot of stuff
