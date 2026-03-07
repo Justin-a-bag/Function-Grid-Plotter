30b9002d916b887fbbd3ef15d72ebd0923d432b4
Potatovender <156006555+Potatovender@users.noreply.github.com>
2026-03-06 23:49:12 -0500

    Update Color.py

--- Color.py
+++ Color.py
@@ -1,2 +1,4 @@
 class Color:
   
+# contains 3 equations and you get to plug in values to return a value for either r, g, or b
+# these special classes aren't anything special they're just kind of wrappers for the data
