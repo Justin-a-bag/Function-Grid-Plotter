d13e3336e3df30f82f5188595e10a4d8f3653bc7
Potatovender <156006555+Potatovender@users.noreply.github.com>
2026-03-06 23:47:26 -0500

    wrote starter notes

--- Equation.py
+++ Equation.py
@@ -1,1 +1,7 @@
 class Equation:
+  #represents one equation f(x,y) = z
+  #contains one tree
+  #evaluate(self,x:float, y:float)->float:
+  #only returns value for a single point
+  #as_string(self)->string:
+  
