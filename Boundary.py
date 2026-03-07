f216b6e2531f401c0ada3ad99dc9cc7b0b52398c
Potatovender <156006555+Potatovender@users.noreply.github.com>
2026-03-06 23:49:49 -0500

    wrote starter message

--- Boundary.py
+++ Boundary.py
@@ -1,2 +1,5 @@
 class Boundary:
   
+
+# contains 1 equation (and maybe a boolean) and you get to plug in values to return if it's in or out of bounds
+# these special classes aren't anything special they're just kind of wrappers for the data
