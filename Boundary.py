05ffd2723cdebb1e244b20f6b455a5d4e3b89ab2
Potatovender <156006555+Potatovender@users.noreply.github.com>
2026-03-09 14:00:29 -0400

    implemented the thing

--- Boundary.py
+++ Boundary.py
@@ -1,5 +1,28 @@
-class Boundary:
-  
+from __future__ import annotations 
+import math
 
-# contains 1 equation (and maybe a boolean) and you get to plug in values to return if it's in or out of bounds
-# these special classes aren't anything special they're just kind of wrappers for the data
+from Equation import Equation
+class Boundary:
+  """
+  A wrapper class that maps values to colors
+  """
+  bounder: Equation
+  checkSmaller: bool
+
+  def __init__(self, boundary = Equation("1"), checkSmaller=False)):
+    self.boundary=boundary
+    self.checkSmaller=checkSmaller
+  def inBounds(self,x:float,y:float)->bool:
+    """
+    returns whether or not the x and y values are in bounds as specified by the boundary provided earlier
+    yeah idk guys it does what it says
+    """
+    #i think the code is pretty readable just like look at it ig
+    squarevalue=bounder.evaluate(x,y)
+    #you guys can decide if we actually want an inclusive/exclusive check for squarevalue right now it's inclusive
+    if checkSmaller==False:
+      return squarevalue>=0
+    else:
+      return squarevalue<=0
+      
+      
