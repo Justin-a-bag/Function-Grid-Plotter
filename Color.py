c283219f95f30dc97e88d04ccbc0f337209be93d
Potatovender <156006555+Potatovender@users.noreply.github.com>
2026-03-09 13:22:20 -0400

    added color system

--- Color.py
+++ Color.py
@@ -1,4 +1,27 @@
+from __future__ import annotations 
+import math
+
+import Equation
 class Color:
-  
-# contains 3 equations and you get to plug in values to return a value for either r, g, or b
-# these special classes aren't anything special they're just kind of wrappers for the data
+  """
+  A wrapper class that maps values to colors
+  """
+  red: Equation
+  green: Equation
+  blue: Equation
+
+  def __init__(self, red: Equation, green: Equation, blue: Equation):
+    self.red=red
+    self.green=green
+    self.blue=blue
+  def getColorTuple(self,zval:float)->tuple:
+    """
+    returns the color tuple that corresponds to the color at any specific location
+    remember that color is a tuple of 3 integers representing rgb values
+    since color drawing uses integers, cast the value to an integer (surely a 1 point drop in color doesn't actually matter riiigghhhttt?)
+    Hey guys uh if you're reading this idk how we should scale the color values 
+    can you guys decide if we should just drop immediately to 0/255 or use a 1-1 mapping system?
+    also if we should check for absolute max/min values and scale values accordingly?
+    """
+    return (int(self.red.evaluate(zval,0)),int(self.green.evaluate(zval,0)),int(self.blue.evaluate(zval,0)))
+
