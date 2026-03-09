0b50bc75f9017a1b0ac01f3d34f0ae6d32f827b6
Potatovender <156006555+Potatovender@users.noreply.github.com>
2026-03-09 13:42:45 -0400

    just more general quality of life stuff

--- Color.py
+++ Color.py
@@ -10,7 +10,7 @@ class Color:
   green: Equation
   blue: Equation
 
-  def __init__(self, red: Equation, green: Equation, blue: Equation):
+  def __init__(self, red = Equation("x"), green = Equation("x"), blue = Equation("x")):
     self.red=red
     self.green=green
     self.blue=blue
@@ -23,5 +23,16 @@ class Color:
     can you guys decide if we should just drop immediately to 0/255 or use a 1-1 mapping system?
     also if we should check for absolute max/min values and scale values accordingly?
     """
-    return (int(self.red.evaluate(zval,0)),int(self.green.evaluate(zval,0)),int(self.blue.evaluate(zval,0)))
+    #doing it this way since we need to check the values of stuff
+    rval,gval,bval=self.red.evaluate(zval,0),self.green.evaluate(zval,0),self.blue.evaluate(zval,0)
+
+    #yeah fuck you for wanting to read the code
+    if any(c==d for c in [rval,gval,bval] for d in ['invalid','nan']):
+        #THIS SHOULD RETURN A BASE NULL VALUE
+        #i'm using black as the base value but you can choose anything 
+        #(i will note some color schemes don't allow black but thats the point lowkey)
+        return (0,0,0)
+    #final casting
+    #fuck marry kill r value, g value, b value?
+    return (int(rval),int(gval),int(bval))
 
