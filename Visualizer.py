da0af07d020c24a883ad878b869013accffe683b
ngjus <justin329547@gmail.com>
2026-03-24 16:57:58 -0400

    added a potential equation rendering with textboxes, will add more later today

--- Visualizer.py
+++ Visualizer.py
@@ -8,9 +8,39 @@ from Color import Color
 from Boundary import Boundary
 
 # Define screen and drawing boundaries
-WIDTH, HEIGHT = 800, 800
-DRAW_MIN_X, DRAW_MAX_X = 0, WIDTH
+WIDTH, HEIGHT = 1100, 800
+DRAW_MIN_X, DRAW_MAX_X = 300, WIDTH
 DRAW_MIN_Y, DRAW_MAX_Y = 0, HEIGHT
+TEXTBOX_WIDTH, TEXTBOX_HEIGHT = 300, 50
+TEXTBOX_COLOR = (210, 210, 210)
+INDENT_COLOR = (180, 180, 180)
+TB_INDT = 5
+
+
+# textbox object
+class Textbox:
+    """
+    a textbox object to condense the information needed to create an iterable, mutable textbox
+    """
+    text: str
+    rect: pygame.Rect
+    color: pygame.Color
+    active = bool
+
+    def __init__(self, new_text: str, new_rect: pygame.Rect, new_color: pygame.Color):
+        self.text = new_text
+        self.rect = new_rect
+        self.color = new_color
+
+    def update_rect(self, new_y: int):
+        if self.rect.y != new_y:
+            self.rect.move(self.rect.x, new_y)
+
+    def update_text(self, passed_ev: pygame.event.Event):
+        if passed_ev.type == pygame.K_BACKSPACE:
+            self.text = self.text[:-1]
+        else:
+            self.text += passed_ev.unicode
 
 
 def render_grid(screen: pygame.Surface, drawFunc: Equation, color: Color, boundary: Boundary, xpoints: list[float],
@@ -33,7 +63,6 @@ def render_grid(screen: pygame.Surface, drawFunc: Equation, color: Color, bounda
                 # 2. Evaluate the equation
                 z = drawFunc.evaluate(math_x, math_y)
 
-
                 # 3. Get the color from your Color class
                 squarecolor = color.getColorTuple(z)
 
@@ -43,8 +72,23 @@ def render_grid(screen: pygame.Surface, drawFunc: Equation, color: Color, bounda
                 screen_y = DRAW_MAX_Y - ((j + 1) * cell_h)
 
                 # 5. Draw it!
-                if squarecolor != (-1,-1,-1):
-                  pygame.draw.rect(screen, squarecolor, (screen_x, screen_y, max(1.0,cell_w), max(1.0,cell_h)))
+                if squarecolor != (-1, -1, -1):
+                    pygame.draw.rect(screen, squarecolor, (screen_x, screen_y, max(1.0,cell_w), max(1.0,cell_h)))
+
+
+def render_textboxes(scr: pygame.Surface, text_lst: list[Textbox], t_index : int) -> None:
+    """
+    Updates the list and pygame rectangle objects associated to each string in the 2d list
+    """
+    # updates the rectangle
+    for i in range(len(text_lst)):
+        text_lst[i].update_rect(TEXTBOX_HEIGHT*i)
+        tb_box = text_lst[i].rect
+        pygame.draw.rect(scr, TEXTBOX_COLOR, text_lst[i].rect)
+        if t_index == i:
+            pygame.draw.rect(scr, INDENT_COLOR, (tb_box.x + TB_INDT, tb_box.y + TB_INDT,
+                                                    tb_box.width - (2 * TB_INDT), tb_box.height - (2 * TB_INDT)))
+
 
 if __name__ == "__main__":
     # --- 1. PYGAME SETUP ---
@@ -59,7 +103,7 @@ if __name__ == "__main__":
     g = Equation("255(sin(1.5(x+pi/2))/2.8+0.5)")
     b = Equation("255(e^(-(3(x+0.99))^2)/3-x/9+0.1)")
     eq2 = Equation("arctan(2sin(-2x-y/8+cos(3y-x-sin(cos(sin( sin(x*y) + x )+x-y+arctan(y)))))+\frac{\left(x^{2}+\frac(y^2,14)+0\right)}{3}-100/(x^2+y^2)+2.71828^(-4-y))")  #
-    my_color = Color(r,g,b)
+    my_color = Color(r, g, b)
     my_boundary = Boundary(Equation("arctan(2sin(-2x-y/8+cos(3y-x-sin(cos(sin( sin(x*y) + x )+x-y+arctan(y)))))+\frac{\left(x^{2}+\frac(y^2,14)+0\right)}{3}-100/(x^2+y^2)+2.71828^(-4-y))-1.45"),True)
     my_boundary = Boundary()
 
@@ -77,26 +121,48 @@ if __name__ == "__main__":
     y_coords2 = [MATH_MIN + j * step for j in range(GRID_RESOLUTION)]
 
     # --- 3. RENDER ONCE ---
-    screen.fill((0, 0, 0))  # Fill background with black
+    screen.fill((255, 255, 255))  # Fill background with black
     print("Rendering grid...")
     renderingtime = GRID_RESOLUTION ** 2 * (eq.size() + r.size() + g.size() + b.size()) / 1000000
     print("estimated rendering time: "+str(renderingtime)+" seconds")
     print("Note: the power of your device will affect runtime speeds.")
 
     render_grid(screen, eq2, my_color, my_boundary, x_coords, y_coords)
-    #render_grid(screen, eq2, my_color, my_boundary, x_coords2, y_coords2)
-      # Pushes the drawing to the actual monitor
+    # render_grid(screen, eq2, my_color, my_boundary, x_coords2, y_coords2)
+    # Pushes the drawing to the actual monitor
     pygame.display.flip()
 
     print("Done!")
 
+
     # --- 4. MAIN EVENT LOOP ---
     # This keeps the window open until you click the red 'X'
+
+    # a 2d list with each index containing [str, pygame.rect]
+    text_box_lst = list()
+
+    # index keeper to make sure that we know which box we are assigned to (will be assigned to the greatest index)
+    # text_index < len(text_box_lst)
+    text_index = 0
+
+
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            # keyboard events
+            if event.type == pygame.KEYDOWN:
+                # text_index has to be smaller than the list amount, will not go outside of it
+                if text_index < len(text_box_lst) and text_index >= 0:
+                    text_box_lst[text_index].update_text(event)     # acts as backspace
+                    render_textboxes(screen, text_box_lst, text_index)
+                pygame.display.flip()
+
+            # mouse click events
+
+
 
     pygame.quit()
     sys.exit()
